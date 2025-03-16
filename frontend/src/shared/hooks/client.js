7608e57b9ff5ec4a118c09f588c74925dad038a0
Olawale Ayobami <148712150+haywhai@users.noreply.github.com>
2025-03-16 01:36:49 +0100

    fix

--- frontend/src/shared/hooks/client.js
+++ frontend/src/shared/hooks/client.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-    baseURL: "http://localhost:4000/api/v1",
+    baseURL: process.env.NODE_ENV === "production"
+    ? "https://rola-restaurant-backend.onrender.com/api/v1"
+    : "http://localhost:4000/api/v1", 
     headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${localStorage.getItem("token")}`,
